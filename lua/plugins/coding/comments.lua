0582860d099d275947b8104b5d4efdc843c2c2e8
mauriba <53594538+mauriba@users.noreply.github.com>
2024-08-07 16:31:31 +0700

    feat: added oil file explorer

---
+++ lua/plugins/coding/comments.lua
@@ -0,0 +1,8 @@
+return {
+    "tpope/vim-commentary",
+    -- Commands:
+    -- "gcc": toggle comment for current line
+    -- "gc": toggle comment for selection (visual mode)
+    -- "gc<movement>": toggle comment for movement, e.g.
+    --     "gcap" to toggle comment around current paragraph
+}
