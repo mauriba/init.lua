d540b893544fd78b1176964a0e1b7cb8810a823f
mauriba <53594538+mauriba@users.noreply.github.com>
2024-08-09 18:16:59 +0700

    feat: remaps for moving lines up and down

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -4,7 +4,13 @@ vim.keymap.set("n", "<leader>tn", function ()
     local directory = require("oil").get_current_dir()
     local Terminal  = require('toggleterm.terminal').Terminal
     local term = Terminal:new({
-        dir = directory
     })
+    dir = directory
     term:toggle()
 end, {desc = "New terminal in current directory"})
+
+-- Remaps for moving line around (:m) and properly intend it (=)
+vim.keymap.set("n", "J", ":m +1<CR>==")
+vim.keymap.set("n", "K", ":m -2<CR>==")
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
