f9206d7caaa5f6fc9b9a6d0d750992cbd6b03026
mauriba <53594538+mauriba@users.noreply.github.com>
2024-10-27 12:00:36 +0100

    fix: fixed :Q not quitting neovim like :q

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -32,3 +32,6 @@ vim.keymap.set("n", "J", ":m +1<CR>==")
 vim.keymap.set("n", "K", ":m -2<CR>==")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+
+-- Let Q also quit neovim
+vim.api.nvim_create_user_command("Q", ":q", {})
