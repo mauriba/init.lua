b36043c043ec5349644e92767209e03072303030
mauriba <53594538+mauriba@users.noreply.github.com>
2024-11-15 10:17:47 +0100

    refactor: move statuscol from folding to plugins/editor/statuscol.lua

--- lua/plugins/coding/folding.lua
+++ lua/plugins/coding/folding.lua
@@ -125,23 +125,6 @@ return {
         "kevinhwang91/nvim-ufo",
         dependencies = {
             "kevinhwang91/promise-async",
-            {
-                "luukvbaal/statuscol.nvim",
-                config = function()
-                    local builtin = require("statuscol.builtin")
-                    require("statuscol").setup({
-                        relculright = true,
-                        segments = {
-                            {
-                                sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-                                click = "v:lua.ScSa"
-                            },
-                            { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
-                            { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
-                        }
-                    })
-                end,
-            },
         },
         event = "BufReadPost",
         opts = {
---
+++ lua/plugins/editor/statuscol.lua
@@ -0,0 +1,17 @@
+return {
+    "luukvbaal/statuscol.nvim",
+    config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+            relculright = true,
+            segments = {
+                {
+                    sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
+                    click = "v:lua.ScSa"
+                },
+                { text = { builtin.lnumfunc },      click = "v:lua.ScLa", },
+                { text = { builtin.foldfunc, " " }, click = "v:lua.ScFa" },
+            }
+        })
+    end,
+}
