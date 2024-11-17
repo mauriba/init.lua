902c8cd261f87b891eb13b6241c7640e044ab8a4
mauriba <53594538+mauriba@users.noreply.github.com>
2024-11-17 13:11:40 +0100

    fix: enable snippet expansion for lua function autocomplete

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -2,4 +2,12 @@ require("config.coding.folding").addCommentStyle("lua", {
     block = { open = "--[[", close = "--]]" },
     line = "--"
 })
-require("config.coding.lspconfig").start("lua_ls", {})
+require("config.coding.lspconfig").start("lua_ls", {
+    settings = {
+        Lua = {
+            completion = {
+                callSnippet = 'Replace',
+            },
+        },
+    },
+})
