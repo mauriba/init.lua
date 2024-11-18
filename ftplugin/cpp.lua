290001046a4ecc4d26277f7e27d02a879914b406
mauriba <53594538+mauriba@users.noreply.github.com>
2024-11-18 11:45:39 +0100

    feat: add dap conifgs and dap support for cpp

--- ftplugin/cpp.lua
+++ ftplugin/cpp.lua
@@ -4,3 +4,27 @@ require("config.coding.folding").addCommentStyle("cpp", {
 })
 require("config.coding.lspconfig").start("clangd", {
 })
+
+local dap = require("dap")
+dap.adapters.codelldb = {
+    type = "server",
+    port = "${port}",
+    host = "127.0.0.1",
+    executable = {
+        command = vim.fn.stdpath("data") .. "/mason/bin/codelldb",
+        args = { "--port", "${port}" },
+    },
+}
+dap.configurations.cpp = {
+    {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+            return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+        end,
+        cwd = "${workspaceFolder}",
+        stopOnEntry = false,
+        runInTerminal = false,
+    },
+}
