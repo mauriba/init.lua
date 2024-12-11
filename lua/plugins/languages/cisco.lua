b50c95722b43abe6b250dca47819fa504621bece
mauriba <53594538+mauriba@users.noreply.github.com>
2024-12-11 09:54:14 +0100

    feat: add syntax highlighting and default settings for cisco commands (#15)
    Put cisco commands in a file named .cisco for it to take effect
    automatically.

---
+++ lua/plugins/languages/cisco.lua
@@ -0,0 +1,9 @@
+return {
+    "momota/cisco.vim",
+    config = function()
+        -- Nothing to do - all this plugin does is adding some legacy vim syntax
+        -- configs that automatically apply to .cisco files.
+    end,
+    ft = { "cisco" },
+}
+
