2cc0760ff1d8fd4443e8ae0af329b4d8cf6f41eb
mauriba <53594538+mauriba@users.noreply.github.com>
2025-01-09 08:25:00 +0100

    feat: gitignore build directories everywhere now

--- samples/haskell/test.hs
+++ samples/haskell/test.hs
@@ -1,18 +1,12 @@
-import GHC.Conc
+-- cd build && ghc -o test.exe -outputdir . ../test.hs && test
+import Prelude hiding (abs)
 
-main = a `par` b `par` c `pseq` print (a + b + c)
-    where
-        a = ack 3 10
-        b = fac 42
-        c = fib 34
+eveno :: Int -> Bool
+eveno n = n `mod` 2 == 0
 
-fac 0 = 1
-fac n = n * fac (n-1)
+noto :: Bool -> String
+noto True = "Number is even"
+noto False = "Number is odd"
 
-ack 0 n = n+1
-ack m 0 = ack (m-1) 1
-ack m n = ack (m-1) (ack m (n-1))
-
-fib 0 = 0
-fib 1 = 1
-fib n = fib (n-1) + fib (n-2)
+main = do
+  print ((noto.eveno) 2)
