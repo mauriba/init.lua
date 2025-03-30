7c50b4526b38a98546fca87859e237d5b21c2372
mauriba <53594538+mauriba@users.noreply.github.com>
2025-03-30 20:47:59 +0200

    feat(lazy): setup lazy and basic colorschemes

---
+++ lua/plugins/colorschemes.lua
@@ -0,0 +1,44 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                background = {
+                    light = "latte",
+                    dark = "mocha"
+                },
+                transparent_background = false,
+                default_integrations = true,
+                integrations = {
+                    cmp = true,
+                    gitsigns = true,
+                    nvimtree = true,
+                    treesitter = true,
+                    notify = false,
+                    mini = {
+                        enabled = true,
+                        indentscope_color = "",
+                    },
+                }
+            })
+        end
+    },
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        config = function()
+            require('rose-pine').setup({
+                disable_background = false,
+                styles = {
+                    italic = false,
+                },
+            })
+        end
+    },
+    {
+        "folke/tokyonight.nvim",
+        opts = { style = "moon" },
+    }
+}
