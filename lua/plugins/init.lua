4f77656ec9f01a01817346baa329508430acdcd8
mauriba <53594538+mauriba@users.noreply.github.com>
2025-03-31 14:56:37 +0200

    feat(coding): add todo-comments (lazy load on files only)

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,5 @@
 return {
     { import = "plugins.editor" },      -- Better editor (gui elements etc.)
     { import = "plugins.practice" },    -- Practice neovim (show/enforce keybinds)
-    -- { import = "plugins.coding" },   -- Improve coding (lsp, treesitter etc.)
+    { import = "plugins.coding" },      -- Improve coding (lsp, treesitter etc.)
 }
