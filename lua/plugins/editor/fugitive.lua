3e15efdf542178fd5a48383d3db4d2a5664b7ce9
mauriba <53594538+mauriba@users.noreply.github.com>
2025-04-05 21:24:40 +0200

    feat(editor): add discord rich presence

--- lua/plugins/editor/fugitive.lua
+++ lua/plugins/editor/fugitive.lua
@@ -2,11 +2,11 @@ return {
     "tpope/vim-fugitive",
     cmd = { "G", "Git", "Gdiffsplit" },
     keys = {
-        { "<leader>gc", "<cmd>G commit<cr>",            desc = "Git commit" },
-        { "<leader>g+", "<cmd>G push<cr>",              desc = "Git push" },
-        { "<leader>g-", "<cmd>G pull --rebase<cr>",     desc = "Git pull" },
-        { "<leader>ga", "<cmd>G add -A<cr>",            desc = "Git add all" },
-        { "<leader>gC", "<cmd>G add -A | G commit<cr>", desc = "Git add all & commit" },
+        { "<leader>gc", "<cmd>silent G commit<cr>",            desc = "Git commit" },
+        { "<leader>g+", "<cmd>silent G push<cr>",              desc = "Git push" },
+        { "<leader>g-", "<cmd>silent G pull --rebase<cr>",     desc = "Git pull" },
+        { "<leader>ga", "<cmd>silent G add -A<cr>",            desc = "Git add all" },
+        { "<leader>gC", "<cmd>silent G add -A | G commit<cr>", desc = "Git add all & commit" },
     },
     config = function()
 
