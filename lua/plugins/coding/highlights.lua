57821378d3ee7d32754e3a995c4fab38f56e6043
mauriba <53594538+mauriba@users.noreply.github.com>
2025-04-05 22:24:27 +0200

    feat(coding): fix highlights for same words in document

--- lua/plugins/coding/highlights.lua
+++ lua/plugins/coding/highlights.lua
@@ -2,7 +2,13 @@ return {
     -- INFO: Comes with <a-n> to go to next, <a-p> to go to previous occurence of word. <a-i> selects the occurence
     {
         "RRethy/vim-illuminate",
-        event = "BufEnter *.*"
+        event = "BufEnter *.*",
+        config = function()
+            require("illuminate").configure({
+                providers = { "treesitter", "regex" },
+                delay = 400,
+            })
+        end,
     },
     {
         'brenoprata10/nvim-highlight-colors',
