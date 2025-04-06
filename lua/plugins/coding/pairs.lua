2eb1b403d8ec9856e6d88340c5df447680c1e906
mauriba <53594538+mauriba@users.noreply.github.com>
2025-04-06 19:34:48 +0200

    feat(markdown): add markdown preview and auto list completion

--- lua/plugins/coding/pairs.lua
+++ lua/plugins/coding/pairs.lua
@@ -3,22 +3,29 @@ return {
         "echasnovski/mini.pairs",
         version = false,
         event = "InsertEnter",
-        config = function ()
+        config = function()
             require('mini.pairs').setup()
         end
     },
     {
         "windwp/nvim-ts-autotag",
         event = "InsertEnter",
-        config = function ()
+        config = function()
             require('nvim-ts-autotag').setup({
                 opts = {
                     -- Defaults
-                    enable_close = true, -- Auto close tags
-                    enable_rename = true, -- Auto rename pairs of tags
+                    enable_close = true,         -- Auto close tags
+                    enable_rename = true,        -- Auto rename pairs of tags
                     enable_close_on_slash = true -- Auto close on trailing </
                 },
             })
         end
     },
+    -- List completions
+    -- Renumber Lists: gN
+    -- Toggle Checkbox: <leader>x
+    {
+        "bullets-vim/bullets.vim",
+        ft = { "markdown", "text" },
+    }
 }
