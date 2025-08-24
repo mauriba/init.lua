430cdd96d3ba9919bead482647cd32e9947505fe
mauriba <53594538+mauriba@users.noreply.github.com>
2025-08-24 09:39:12 +0200

    feat!: remove plugins rarely used

--- lua/plugins/coding/highlights.lua
+++ lua/plugins/coding/highlights.lua
@@ -1,18 +1,9 @@
 return {
-    -- INFO: Comes with <a-n> to go to next, <a-p> to go to previous occurence of word. <a-i> selects the occurence
-    {
-        "RRethy/vim-illuminate",
-        event = "VeryLazy",
-        config = function()
-            require("illuminate").configure({
-                providers = { "treesitter", "regex" },
-                delay = 400,
-            })
-        end,
-    },
     {
         'brenoprata10/nvim-highlight-colors',
-        event = "VeryLazy",
+        ft = {
+            "css", "javascript", "html"
+        },
         config = function()
             require('nvim-highlight-colors').setup({
                 render = "virtual"
