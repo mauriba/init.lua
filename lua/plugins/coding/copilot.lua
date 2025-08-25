5baab41ab46ea3a7f35b0c974f59f858f566b54a
mauriba <53594538+mauriba@users.noreply.github.com>
2025-08-25 20:12:32 +0200

    feat(copilot): add copilot chat and better copilot on/off switch

--- lua/plugins/coding/copilot.lua
+++ lua/plugins/coding/copilot.lua
@@ -1,21 +1,45 @@
--- TODO: Better autocomplete
 return {
-    "zbirenbaum/copilot.lua",
-    cmd = { "Copilot" },
-    config = function()
-        require('copilot').setup({
-            panel = {
-                enabled = false,
+    {
+        "zbirenbaum/copilot.lua",
+        cmd = { "Copilot" },
+        keys = {
+            {
+                "<leader>ct",
+                "<cmd>Copilot toggle<CR>",
+                desc = "Toggle Copilot",
+                mode = "n",
             },
-            suggestion = {
-                enabled = false,
-            },
-            filetypes = {
-                ["."] = false,
-                ["*"] = true,
-            },
-            copilot_node_command = 'node', -- Node.js version must be > 18.x
-            server_opts_overrides = {},
-        })
-    end,
+        },
+        config = function()
+            require('copilot').setup({
+                panel = {
+                    enabled = false,
+                },
+                suggestion = {
+                    enabled = false,
+                    auto_trigger = false
+                },
+                filetypes = {
+                    ["."] = false,
+                    ["*"] = true,
+                },
+                copilot_node_command = 'node', -- Node.js version must be > 18.x
+                server_opts_overrides = {},
+            })
+        end,
+    },
+    {
+        "CopilotC-Nvim/CopilotChat.nvim",
+        dependencies = {
+            { "nvim-lua/plenary.nvim", branch = "master" },
+        },
+        cmd = { "CopilotChat" },
+        build = "make tiktoken",
+        opts = {
+            -- See Configuration section for options
+        },
+        keys = {
+            { "<leader>cc", "<cmd>CopilotChatToggle<CR>", desc = "Toggle CopilotChat" },
+        },
+    }
 }
