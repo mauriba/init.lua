21af04f0a2851eb8fbf0da29d8e2b64eb7468f07
mauriba <53594538+mauriba@users.noreply.github.com>
2025-10-03 23:46:56 +0200

    feat(debugger): add working cpp debuggers

--- lua/plugins/coding/debugger.lua
+++ lua/plugins/coding/debugger.lua
@@ -35,5 +35,77 @@ return {
         vim.keymap.set("n", "<leader>dr", dap.continue)
         vim.keymap.set("n", "<leader>dc", dap.run_to_cursor)
         vim.keymap.set("n", "<leader>dk", dap.step_back)
+
+        -- Use VSCode's C/C++ extension on windows
+        if vim.fn.has("win32") == 1 then
+            -- Find debugger installed by VSCode's cpptools extension
+            local base = os.getenv("USERPROFILE") .. "\\.vscode\\extensions"
+            local matches = vim.fn.glob(base .. "\\ms-vscode.cpptools-*-win32-x64", 0, 1)
+
+            local command = "vscode"
+            if #matches > 0 then
+                -- pick the latest version by sorting alphabetically
+                table.sort(matches)
+                local cpptools_dir = matches[#matches]
+                command = cpptools_dir .. "\\debugAdapters\\bin\\OpenDebugAD7.exe"
+            end
+
+            dap.adapters.vscode = {
+                id = 'cppdbg',
+                type = 'executable',
+                command = command,
+                options = {
+                    detached = false
+                },
+                setupCommands = {
+                    {
+                        text = '-enable-pretty-printing',
+                        description = 'enable pretty printing',
+                        ignoreFailures = false
+                    },
+                },
+            }
+        end
+
+
+        local codelldb = vim.fn.stdpath("data") .. "/mason/packages/codelldb/extension/adapter/codelldb"
+        dap.adapters.codelldb = {
+            type = 'server',
+            port = '${port}',
+            executable = {
+                command = codelldb,
+                args = { '--port', '${port}' },
+            },
+        }
+
+        dap.configurations.cpp = {
+            {
+                name = "Launch VSCode's cpptools",
+                type = "vscode",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = true,
+                runInTerminal = false,
+                MIMode = "lldb",
+                miDebuggerPath = "lldb"
+            },
+            {
+                name = "Launch codelldb",
+                type = "codelldb",
+                request = "launch",
+                program = function()
+                    return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                end,
+                cwd = "${workspaceFolder}",
+                stopOnEntry = false,
+                initCommands = {
+                    -- "settings set target.process.stop-on-sharedlibrary-loads false",
+                    -- "settings set target.process.stop-on-exec false",
+                },
+            },
+        }
     end
 }
