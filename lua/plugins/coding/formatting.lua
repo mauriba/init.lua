cad43411fd27a95545a14cf9da60ea01f404a824
Mauricé Bärisch <Maurice.Baerisch@qbeyond.de>
2025-12-09 16:22:28 +0100

    feat: toggle format on save with <leader>fa

--- lua/plugins/coding/formatting.lua
+++ lua/plugins/coding/formatting.lua
@@ -23,11 +23,6 @@ return {
             c = { "clang-format" },
             json = { "fixjson", "jq", stop_after_first = true }, -- For manual indentation, use ":%!jq --indent 2 ."
         },
-        format_on_save = {
-            -- These options will be passed to conform.format()
-            timeout_ms = 4000,
-            lsp_format = "fallback",
-        },
         -- Set default options
         default_format_opts = {
             lsp_format = "fallback",
@@ -35,5 +30,28 @@ return {
     },
     init = function()
         vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+        -- Autosave toggle
+        _G.conform_autosave = true
+        vim.keymap.set("n", "<leader>fa", function()
+            _G.conform_autosave = not _G.conform_autosave
+            if _G.conform_autosave then
+                print("Format on save: ON")
+            else
+                print("Format on save: OFF")
+            end
+        end, { desc = "Toggle Conform auto-save formatting" })
+
+        -- Autosave functionality
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            callback = function()
+                if _G.conform_autosave then
+                    require("conform").format({
+                        timeout_ms = 4000,
+                        lsp_format = "fallback"
+                    })
+                end
+            end,
+        })
     end
 }
